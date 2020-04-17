badce0354debf8989c571cf984035e3e0bc2467d
Axel C <ac@insightapps.io>
2020-04-18 00:19:16 +0700

    add action types, redirection to homepage after signin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
 import { connect } from 'react-redux';
@@ -11,7 +11,7 @@ import SignInSignUp from './pages/signin-signup/signin-signup.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 
-const App = ({ setCurrentUser }) => {
+const App = ({ currentUser, setCurrentUser }) => {
   useEffect(() => {
     let cleanupSnapShot = null;
     const cleanupAuth = auth.onAuthStateChanged(async (userAuth) => {
@@ -31,22 +31,29 @@ const App = ({ setCurrentUser }) => {
       if (cleanupSnapShot) cleanupSnapShot();
       cleanupAuth();
     };
-  });
+  }, [setCurrentUser]);
 
   return (
     <div>
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInSignUp} />
+        <Route path='/shop' component={ShopPage} />
+        <Route
+          exact
+          path='/signin'
+          render={() => (currentUser ? <Redirect to='/' /> : <SignInSignUp />)}
+          //component={SignInSignUp}
+        />
       </Switch>
     </div>
   );
 };
 
+const mapStateToProps = ({ user }) => ({ currentUser: user.currentUser });
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
